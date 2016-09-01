3486fedfe34af937e4cdd325f7d4e2e3197d91ae
dspman <793495478@qq.com>
2016-09-01 17:28:20 +0800

    消息-单聊-内容静态展示；

--- app/src/main/java/com/mkch/youshi/activity/ChatActivity.java
+++ app/src/main/java/com/mkch/youshi/activity/ChatActivity.java
@@ -1,13 +1,21 @@
 package com.mkch.youshi.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.mkch.youshi.R;
+import com.mkch.youshi.adapter.ChartListAdapter;
+import com.mkch.youshi.bean.ChatBean;
+import com.mkch.youshi.util.CommonUtil;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
@@ -30,7 +38,19 @@ public class ChatActivity extends BaseActivity {
     private ImageView mIvPsInfo;//查看用户
 
     @ViewInject(R.id.iv_chat_go_voice)
-    private ImageView mIvGoVoice;//语音
+    private ImageView mIvGoVoice;//去使用语音
+
+    @ViewInject(R.id.line_keyboard_comptonts_use)
+    private LinearLayout mLineKeybordBlock;//键盘输入框和表情选择
+
+    @ViewInject(R.id.btn_chat_voice_use)
+    private Button mBtnUseVoice;
+
+    @ViewInject(R.id.iv_chat_go_keyboard)
+    private ImageView mIvGoKeyboard;//去使用文字
+
+    @ViewInject(R.id.et_chat_input)
+    private EditText mEtChatInput;//聊天框
 
     @ViewInject(R.id.iv_chat_go_expression)
     private ImageView mIvGoExpression;//表情
@@ -38,13 +58,40 @@ public class ChatActivity extends BaseActivity {
     @ViewInject(R.id.iv_chat_go_more_action)
     private ImageView mIvGoMoreAction;//更多操作
 
+    @ViewInject(R.id.line_chat_more_action_block)
+    private LinearLayout mLineMoreAction;//更多操作-面板弹出
+
     @ViewInject(R.id.gv_chat_more_action)
     private GridView mGvMoreAction;//更多操作-网格视图
+
+    /*
+    聊天
+     */
+    @ViewInject(R.id.rv_chart_list)
+    private RecyclerView mRvList;
+    private String m_jid;
+    //数据
+    private List<ChatBean> m_chart_list;
+    private ChartListAdapter m_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initData();
+        setListener();
+    }
+
+    private void setListener() {
+        mEtChatInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                //当获取焦点时，只要有其他面板在打开状态，隐藏这些面板，差表情面板未设计
+                if (hasFocus){
+                    mLineMoreAction.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 
     private void initData() {
@@ -69,10 +116,24 @@ public class ChatActivity extends BaseActivity {
         //设置适配器
         mGvMoreAction.setAdapter(_adapter);
 
-
+        //聊天内容列表
+        //布局管理器
+        LinearLayoutManager _layout_manager = new LinearLayoutManager(this);
+        mRvList.setLayoutManager(_layout_manager);
+        //初始化list聊天数据
+        m_chart_list = new ArrayList<>();
+        m_chart_list.add(new ChatBean("张三","你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊你好啊",ChatBean.MESSAGE_TYPE_IN,"2016-09-01 14:50"));
+        m_chart_list.add(new ChatBean("张三","在？",ChatBean.MESSAGE_TYPE_IN,"2016-09-02 15:10"));
+        m_chart_list.add(new ChatBean("张三","在吗",ChatBean.MESSAGE_TYPE_IN,"2016-09-03 14:23"));
+        m_chart_list.add(new ChatBean("jsjlj","在的",ChatBean.MESSAGE_TYPE_OUT,"2016-09-03 14:25"));
+        //适配器
+        m_adapter = new ChartListAdapter(m_chart_list);
+        mRvList.setAdapter(m_adapter);
+        mRvList.smoothScrollToPosition(m_chart_list.size());//滚动到最下面
     }
 
-    @Event({R.id.iv_chat_topbar_back, R.id.iv_chat_topbar_ps})
+    @Event({R.id.iv_chat_topbar_back, R.id.iv_chat_topbar_ps,R.id.iv_chat_go_voice,R.id.iv_chat_go_keyboard,R.id.iv_chat_go_expression,R.id.iv_chat_go_more_action
+            ,R.id.et_chat_input})
     private void clickOneButton(View view) {
         switch (view.getId()) {
         case R.id.iv_chat_topbar_back://返回按钮
@@ -81,13 +142,52 @@ public class ChatActivity extends BaseActivity {
         case R.id.iv_chat_topbar_ps://查看用户
             break;
         case R.id.iv_chat_go_voice://切换到语音
+            changeKeyboardAndVoice();
+            break;
+        case R.id.iv_chat_go_keyboard://切换到键盘
+            changeKeyboardAndVoice();
+            break;
+        case R.id.et_chat_input://点击输入框
+            mLineMoreAction.setVisibility(View.GONE);//隐藏面板-差表情面板
+            mRvList.smoothScrollToPosition(m_chart_list.size());//滚动到最下面
             break;
         case R.id.iv_chat_go_expression://弹出表情
             break;
-        case R.id.iv_chat_go_more_action://弹出更多操作
+        case R.id.iv_chat_go_more_action://切换弹出更多操作
+            if (mLineMoreAction.getVisibility()==View.VISIBLE){
+                mLineMoreAction.setVisibility(View.GONE);
+            }else {
+                mLineMoreAction.setVisibility(View.VISIBLE);
+                CommonUtil.hideInput(this,mEtChatInput);//隐藏输入法
+            }
+            mRvList.smoothScrollToPosition(m_chart_list.size());//滚动到最下面
             break;
     }
 }
+
+    /**
+     * 切换键盘和语音
+     */
+    private void changeKeyboardAndVoice() {
+        if (mLineKeybordBlock.getVisibility()== View.VISIBLE){
+            //点击了语音
+            CommonUtil.hideInput(this,mEtChatInput);//隐藏输入法
+
+            mIvGoVoice.setVisibility(View.GONE);
+            mIvGoKeyboard.setVisibility(View.VISIBLE);
+
+            mLineKeybordBlock.setVisibility(View.GONE);
+            mBtnUseVoice.setVisibility(View.VISIBLE);
+        }else {
+            //点击了键盘
+            mIvGoVoice.setVisibility(View.VISIBLE);
+            mIvGoKeyboard.setVisibility(View.GONE);
+
+            mLineKeybordBlock.setVisibility(View.VISIBLE);
+            mBtnUseVoice.setVisibility(View.GONE);
+        }
+        mLineMoreAction.setVisibility(View.GONE);//隐藏面板-差表情面板
+    }
 
 
 }
