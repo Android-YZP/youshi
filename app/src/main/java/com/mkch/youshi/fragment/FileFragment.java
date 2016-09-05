50ca11523b6d41afd2033665a284d903df2fbdc6
zzzmfx <2885623312@qq.com>
2016-09-05 16:04:25 +0800

    我的收藏界面实现，点击预览、长按更多操作的跳转实现

--- app/src/main/java/com/mkch/youshi/fragment/FileFragment.java
+++ app/src/main/java/com/mkch/youshi/fragment/FileFragment.java
@@ -1,15 +1,22 @@
 package com.mkch.youshi.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.mkch.youshi.R;
+import com.mkch.youshi.activity.FilePreviewActivity;
+import com.mkch.youshi.activity.PhoneContactsActivity;
+import com.mkch.youshi.activity.UserInformationActivity;
 import com.mkch.youshi.adapter.AddFriendsMethodsListAdapter;
 import com.mkch.youshi.adapter.FileListAdapter;
 
@@ -17,6 +24,8 @@ public class FileFragment extends Fragment {
 
     private SwipeRefreshLayout mSRLayout;
     private ListView mListView;
+    //保存选择的性别
+    private String[] operation_list = {"下载", "转发", "删除"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,6 +44,7 @@ public class FileFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initData();
+        setListener();
     }
 
     @Override
@@ -45,5 +55,33 @@ public class FileFragment extends Fragment {
     private void initData() {
         ListAdapter mAdapter = new FileListAdapter(getActivity());
         mListView.setAdapter(mAdapter);
+    }
+
+    private void setListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent _intent = null;
+                switch (position){
+                    case 0:
+                        _intent = new Intent(getActivity(),FilePreviewActivity.class);
+                        startActivity(_intent);
+                        break;
+                }
+            }
+        });
+
+        //长按文件或收藏，弹出对话框选择操作
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                new AlertDialog.Builder(getActivity()).setItems(operation_list, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
+                return true;
+            }
+        });
     }
 }
