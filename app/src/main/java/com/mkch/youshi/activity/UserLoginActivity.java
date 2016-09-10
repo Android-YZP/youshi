cb1ebc54b3ef8a7d9e0759035e0dabbc8f206dd2
zzzmfx <2885623312@qq.com>
2016-09-10 16:33:48 +0800

    登录功能的基本完善

--- app/src/main/java/com/mkch/youshi/activity/UserLoginActivity.java
+++ app/src/main/java/com/mkch/youshi/activity/UserLoginActivity.java
@@ -23,6 +23,7 @@ import com.mkch.youshi.bean.HttpResultBean;
 import com.mkch.youshi.bean.LoginUserJson;
 import com.mkch.youshi.bean.User;
 import com.mkch.youshi.config.CommonConstants;
+import com.mkch.youshi.util.CheckUtil;
 import com.mkch.youshi.util.CommonUtil;
 
 import org.apache.http.conn.ConnectTimeoutException;
@@ -43,13 +44,13 @@ public class UserLoginActivity extends Activity {
 
     private EditText mEtAccount;//用户名
     private EditText mEtPassword;//密码
-    private LinearLayout mLayoutCode;
+    private String mAccount;//手机号
+    private LinearLayout mLayoutCode;//控制验证码是否显示
     private EditText mEtCode;//验证码
     private ImageView mIvCode;//验证码图片
     private Button mBtnLogin;//登录按钮
     private TextView mTvGoRegister;//去注册
     private TextView mTvGoForgot;//去忘记密码
-
     private static ProgressDialog mProgressDialog = null;//登录加载
     private User mUser;//用户信息
     private String mPicUrl;//图片验证码地址
@@ -71,10 +72,11 @@ public class UserLoginActivity extends Activity {
         mBtnLogin = (Button) findViewById(R.id.btn_user_login_commit);
         mTvGoRegister = (TextView) findViewById(R.id.tv_user_login_reg);
         mTvGoForgot = (TextView) findViewById(R.id.tv_user_login_forget);
-//        mLayoutCode.setVisibility(View.GONE);
+        mLayoutCode.setVisibility(View.GONE);
     }
 
     private void setListener() {
+        mIvCode.setOnClickListener(new UserLoginOnClickListener());
         mBtnLogin.setOnClickListener(new UserLoginOnClickListener());
         mTvGoRegister.setOnClickListener(new UserLoginOnClickListener());
         mTvGoForgot.setOnClickListener(new UserLoginOnClickListener());
@@ -103,13 +105,21 @@ public class UserLoginActivity extends Activity {
                     //登录成功
                     ((UserLoginActivity) mActivity.get()).goMain();
                     break;
+                case CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_SHOW:
+                    //图片验证码出现
+                    ((UserLoginActivity) mActivity.get()).showImgVerfy();
+                    break;
+                case CommonConstants.FLAG_NO_GET_PICCODE:
+                    //不需要图片验证码，直接发起登录
+                    ((UserLoginActivity) mActivity.get()).sendLoginFromNet();
+                    break;
                 case CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_ERROR:
                     //图片验证码错误
                     ((UserLoginActivity) mActivity.get()).imgVerifyError();
                     break;
-                case CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_SHOW:
-                    //图片验证码出现
-                    ((UserLoginActivity) mActivity.get()).showImgVerfy();
+                case CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_CHAGE:
+                    //切换图片验证码
+                    ((UserLoginActivity) mActivity.get()).changeImgVerfy();
                     break;
                 default:
                     break;
@@ -117,14 +127,24 @@ public class UserLoginActivity extends Activity {
         }
     }
 
+    private MyHandler handler = new MyHandler(this);
+
+    /**
+     * 出现图片验证码
+     */
+    private void showImgVerfy() {
+        mLayoutCode.setVisibility(View.VISIBLE);
+        Toast.makeText(UserLoginActivity.this, "手机号或密码错误，请输入验证码", Toast.LENGTH_SHORT).show();
+        x.image().bind(mIvCode, mPicUrl);
+    }
+
     /**
      * 图片验证码错误-提示并重刷图片验证码
      */
     private void imgVerifyError() {
-        this.showTip("图片验证码错误");
+        this.showTip("验证码错误");
         //把ImageView重新刷新图片验证码
         changeImgVerfy();//更新图片验证码
-
     }
 
     /**
@@ -135,14 +155,14 @@ public class UserLoginActivity extends Activity {
     }
 
     /**
-     * 出现图片验证码
+     * 发起登录请求
      */
-    private void showImgVerfy() {
-        Toast.makeText(UserLoginActivity.this, "密码错误，请重新输入", Toast.LENGTH_SHORT).show();
-        x.image().bind(mIvCode, mPicUrl);
+    private void sendLoginFromNet() {
+        String account = mEtAccount.getText().toString();
+        String password = mEtPassword.getText().toString();
+        String code = mEtCode.getText().toString();
+        userLoginFromNet(account,password,code);
     }
-
-    private MyHandler handler = new MyHandler(this);
 
     private void showTip(String str) {
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
@@ -160,16 +180,65 @@ public class UserLoginActivity extends Activity {
         startActivity(_intent);
     }
 
-    private class UserLoginOnClickListener implements OnClickListener {
+    /**
+     * 检查是否需要图片验证码
+     */
+    protected void checkIsNeedValidImgFromNet() {
+        mAccount = mEtAccount.getText().toString();
+        //检查是否需要图片验证码
+        RequestParams requestParams = new RequestParams(CommonConstants.CheckLoginPickCodeExist);
+        //包装请求参数
+        String _req_json = "{\"sMobileNumber\":\"" + mAccount + "\"}";
+        requestParams.addBodyParameter("", _req_json);//用户名
+        requestParams.addHeader("sVerifyCode", "3D8829FE");//头信息
+        x.http().post(requestParams, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                if (result != null) {
+                    try {
+                        JSONObject _json_result = new JSONObject(result);
+                        Boolean _success = (Boolean) _json_result.get("Success");
+                        if (_success) {
+                            JSONObject datas = _json_result.getJSONObject("Datas");
+                            mPicUrl = CommonConstants.NOW_ADDRESS_PRE + datas.getString("PicCodePath");
+                            handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_SHOW);
+                        }else{
+                            handler.sendEmptyMessage(CommonConstants.FLAG_NO_GET_PICCODE);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+            }
+
+            @Override
+            public void onFinished() {
+            }
+        });
+    }
+
+    /**
+     * 自定义点击监听类
+     */
+    private class UserLoginOnClickListener implements OnClickListener {
         @Override
         public void onClick(View view) {
             Intent _intent = null;
             switch (view.getId()) {
+                case R.id.iv_user_login_code:
+                    changePicCodeFromNet();
+                    break;
                 case R.id.btn_user_login_commit:
                     String account = mEtAccount.getText().toString();
                     String password = mEtPassword.getText().toString();
-                    String code = mEtCode.getText().toString();
                     if (account == null || account.equals("")) {
                         Toast.makeText(UserLoginActivity.this, "您未填写手机号", Toast.LENGTH_SHORT).show();
                         return;
@@ -178,10 +247,11 @@ public class UserLoginActivity extends Activity {
                         Toast.makeText(UserLoginActivity.this, "您未填写密码", Toast.LENGTH_SHORT).show();
                         return;
                     }
-                    //弹出加载进度条
-                    mProgressDialog = ProgressDialog.show(UserLoginActivity.this, "请稍等", "正在登录中...", true, true);
-                    //发起网络登录
-                    userLoginFromNet(account, password, code);
+                    if (!CheckUtil.checkMobile(account)) {
+                        Toast.makeText(UserLoginActivity.this, "手机号格式输入有误", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    checkIsNeedValidImgFromNet();
                     break;
                 case R.id.tv_user_login_reg:
                     _intent = new Intent(UserLoginActivity.this, UserRegPhoneActivity.class);
@@ -195,98 +265,137 @@ public class UserLoginActivity extends Activity {
                     break;
             }
         }
+    }
 
-        /**
-         * 发起网络登录
-         *
-         * @param account  账号
-         * @param password 密码
-         */
-        private void userLoginFromNet(final String account, final String password, final String code) {
-            //使用xutils3访问网络并获取返回值
-            RequestParams requestParams = new RequestParams(CommonConstants.LOGIN);
-            //包装请求参数
-            User user = new User();
-            user.setMobileNumber(account);
-            user.setPassword(password);
-            user.setClientType("Android");
-            user.setImageVerifyCode(code);
-            user.setOsUuid(CommonUtil.getMyUUID(UserLoginActivity.this));
-            LoginUserJson _login_user = new LoginUserJson(user);
-            final Gson gson = new Gson();
-            String _user_json = gson.toJson(_login_user);
-            Log.d("jlj", "_user_json is ----------------" + _user_json);
-            requestParams.addBodyParameter("", _user_json);//用户名
-            requestParams.addHeader("sVerifyCode", "3D8829FE");//头信息
-            x.http().post(requestParams, new Callback.CommonCallback<String>() {
-                @Override
-                public void onSuccess(String result) {
-                    if (result != null) {
-                        Log.d("userLogin", "----onSuccess:" + result);
-                        //若result返回信息中登录成功，解析json数据并存于本地，再使用handler通知UI更新界面并进行下一步逻辑
-                        try {
-                            JSONObject _json_result = new JSONObject(result);
-                            Boolean _success = (Boolean) _json_result.get("Success");
-                            if (_success) {
-                                //填充本地用户信息
-                                String _Datas = _json_result.getString("Datas");
-                                mUser = gson.fromJson(_Datas, User.class);
-                                if (mUser != null) {
-                                    Log.d("jlj", "-------mUser = " + mUser.toString());
-                                }
-                                //提醒登录成功
-                                handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_SUCCESS);
-                            } else {
-                                String _Message = _json_result.getString("Message");
-                                String _ErrorCode = _json_result.getString("ErrorCode");
-                                if (_ErrorCode != null && _ErrorCode.equals("1004")) {
-                                    String _Datas = _json_result.getString("Datas");
-                                    JSONObject _Datas_jsonobj = new JSONObject(_Datas);
-                                    mPicUrl = CommonConstants.NOW_ADDRESS_PRE + _Datas_jsonobj.getString("PicCodePath");
-                                    Log.d("jlj", "-------------------图片验证码错误--------------" + mPicUrl);
-                                    handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_ERROR);
-                                } else if (_ErrorCode != null && _ErrorCode.equals("1006")) {
-                                    String _Datas = _json_result.getString("Datas");
-                                    JSONObject _Datas_jsonobj = new JSONObject(_Datas);
-                                    mPicUrl = CommonConstants.NOW_ADDRESS_PRE + _Datas_jsonobj.getString("PicCodePath");
-                                    Log.d("jlj", "-------------------图片验证码出现--------------" + mPicUrl);
-                                    handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_SHOW);
-                                } else {
-                                    CommonUtil.sendErrorMessage(_Message, handler);
-                                }
+    /**
+     * 发起网络登录
+     *
+     * @param account  账号
+     * @param password 密码
+     */
+    private void userLoginFromNet(final String account, final String password, final String code) {
+        //弹出加载进度条
+        mProgressDialog = ProgressDialog.show(UserLoginActivity.this, "请稍等", "正在登录中...", true, true);
+        //使用xutils3访问网络并获取返回值
+        RequestParams requestParams = new RequestParams(CommonConstants.LOGIN);
+        //包装请求参数
+        User user = new User();
+        user.setMobileNumber(account);
+        user.setPassword(password);
+        user.setClientType("Android");
+        user.setImageVerifyCode(code);
+        user.setOsUuid(CommonUtil.getMyUUID(UserLoginActivity.this));
+        LoginUserJson _login_user = new LoginUserJson(user);
+        final Gson gson = new Gson();
+        String _user_json = gson.toJson(_login_user);
+        Log.d("jlj", "_user_json is ----------------" + _user_json);
+        requestParams.addBodyParameter("", _user_json);//用户名
+        requestParams.addHeader("sVerifyCode", "3D8829FE");//头信息
+        x.http().post(requestParams, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                if (result != null) {
+                    Log.d("userLogin", "----onSuccess:" + result);
+                    //若result返回信息中登录成功，解析json数据并存于本地，再使用handler通知UI更新界面并进行下一步逻辑
+                    try {
+                        JSONObject _json_result = new JSONObject(result);
+                        Boolean _success = (Boolean) _json_result.get("Success");
+                        if (_success) {
+                            //填充本地用户信息
+                            String _Datas = _json_result.getString("Datas");
+                            mUser = gson.fromJson(_Datas, User.class);
+                            if (mUser != null) {
+                                Log.d("jlj", "-------mUser = " + mUser.toString());
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                            //提醒登录成功
+                            handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_SUCCESS);
+                        } else {
+                            String _Message = _json_result.getString("Message");
+                            String _ErrorCode = _json_result.getString("ErrorCode");
+                            if (_ErrorCode != null && _ErrorCode.equals("1004")) {
+                                String _Datas = _json_result.getString("Datas");
+                                JSONObject _Datas_jsonobj = new JSONObject(_Datas);
+                                mPicUrl = CommonConstants.NOW_ADDRESS_PRE + _Datas_jsonobj.getString("PicCodePath");
+                                Log.d("jlj", "-------------------图片验证码错误--------------" + mPicUrl);
+                                handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_ERROR);
+                            } else {
+                                CommonUtil.sendErrorMessage(_Message, handler);
+                            }
                         }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 }
+            }
 
-                @Override
-                public void onError(Throwable ex, boolean isOnCallback) {
-                    Log.d("jlj", "-------onError = " + ex.getMessage());
-                    //使用handler通知UI提示用户错误信息
-                    if (ex instanceof ConnectException) {
-                        CommonUtil.sendErrorMessage(CommonConstants.MSG_CONNECT_ERROR, handler);
-                    } else if (ex instanceof ConnectTimeoutException) {
-                        CommonUtil.sendErrorMessage(CommonConstants.MSG_CONNECT_TIMEOUT, handler);
-                    } else if (ex instanceof SocketTimeoutException) {
-                        CommonUtil.sendErrorMessage(CommonConstants.MSG_SERVER_TIMEOUT, handler);
-                    } else {
-                        CommonUtil.sendErrorMessage(CommonConstants.MSG_DATA_EXCEPTION, handler);
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.d("jlj", "-------onError = " + ex.getMessage());
+                //使用handler通知UI提示用户错误信息
+                if (ex instanceof ConnectException) {
+                    CommonUtil.sendErrorMessage(CommonConstants.MSG_CONNECT_ERROR, handler);
+                } else if (ex instanceof ConnectTimeoutException) {
+                    CommonUtil.sendErrorMessage(CommonConstants.MSG_CONNECT_TIMEOUT, handler);
+                } else if (ex instanceof SocketTimeoutException) {
+                    CommonUtil.sendErrorMessage(CommonConstants.MSG_SERVER_TIMEOUT, handler);
+                } else {
+                    CommonUtil.sendErrorMessage(CommonConstants.MSG_DATA_EXCEPTION, handler);
+                }
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.d("userLogin", "----onCancelled");
+            }
+
+            @Override
+            public void onFinished() {
+                Log.d("userLogin", "----onFinished");
+                //使用handler通知UI取消进度加载对话框
+            }
+        });
+    }
+
+    /**
+     * 切换登录图片验证码
+     */
+    private void changePicCodeFromNet() {
+        String phone = mEtAccount.getText().toString();
+        //使用xutils3访问网络并获取返回值
+        RequestParams requestParams = new RequestParams(CommonConstants.ReloadLoginPicCode );
+        //包装请求参数
+        String _req_json = "{\"sMobileNumber\":\"" + phone + "\"}";
+        requestParams.addBodyParameter("", _req_json);//用户名
+        requestParams.addHeader("sVerifyCode", "3D8829FE");//头信息
+        x.http().post(requestParams, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                if (result != null) {
+                    try {
+                        JSONObject _json_result = new JSONObject(result);
+                        Boolean _success = (Boolean) _json_result.get("Success");
+                        if (_success) {
+                            JSONObject datas = _json_result.getJSONObject("Datas");
+                            mPicUrl = CommonConstants.NOW_ADDRESS_PRE + datas.getString("PicCodePath");
+                            handler.sendEmptyMessage(CommonConstants.FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_CHAGE);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 }
+            }
 
-                @Override
-                public void onCancelled(CancelledException cex) {
-                    Log.d("userLogin", "----onCancelled");
-                }
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+            }
 
-                @Override
-                public void onFinished() {
-                    Log.d("userLogin", "----onFinished");
-                    //使用handler通知UI取消进度加载对话框
-                }
-            });
-        }
+            @Override
+            public void onCancelled(CancelledException cex) {
+            }
+
+            @Override
+            public void onFinished() {
+            }
+        });
     }
 }
--- app/src/main/java/com/mkch/youshi/config/CommonConstants.java
+++ app/src/main/java/com/mkch/youshi/config/CommonConstants.java
@@ -13,6 +13,8 @@ public class CommonConstants {
 	public static final int FLAG_GET_USER_JOIN_IMG_VERIFY_SHOW = 5;
 	public static final int FLAG_GET_USER_SEND_VERIFICATION_CODE = 6;
 	public static final int FLAG_GET_REG_MOBILEMGS_VALIDATE_CAN_GET_AGAIN_SUCCESS = 7;
+	public static final int FLAG_NO_GET_PICCODE = 8;
+	public static final int FLAG_GET_REG_USER_LOGIN_IMG_VERIFY_CHAGE = 9;
 	/**
      * yaozhongping
      * 从100开始
@@ -47,13 +49,17 @@ public class CommonConstants {
 	 */
 	//登录
 	public static final String LOGIN = NOW_ADDRESS + "Login";
+	//登录是否需要图片验证码
+	public static final String CheckLoginPickCodeExist  = NOW_ADDRESS + "CheckLoginPickCodeExist";
+	//切换登录图片验证码
+	public static final String ReloadLoginPicCode   = NOW_ADDRESS + "ReloadLoginPicCode ";
     //注册
 	public static final String JOIN = NOW_ADDRESS + "Join";
 	//获取TokenID
 	public static final String GetTokenID = NOW_ADDRESS + "GetTokenID";
-	//是否需要图片验证码
+	//注册是否需要图片验证码
 	public static final String CheckMessagePickCodeExist = NOW_ADDRESS + "CheckMessagePickCodeExist";
-	//是否需要图片验证码
+	//判断手机号是否已注册
 	public static final String MobileNumberIsExist = NOW_ADDRESS + "MobileNumberIsExist";
 	//发送短信验证码
 	public static final String SetVerificationCode  = NOW_ADDRESS + "SetVerificationCode";
