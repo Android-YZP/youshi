d1c31546e1273bba2dd7d212014041ba303f81f2
zzzmfx <2885623312@qq.com>
2016-09-23 10:41:29 +0800

    1.地区和昵称长度显示的优化 2.头像更换后自动转换成圆形的优化 3.通用界面的优化

--- app/src/main/java/com/mkch/youshi/activity/ReviseAddressActivity.java
+++ app/src/main/java/com/mkch/youshi/activity/ReviseAddressActivity.java
@@ -1,7 +1,11 @@
 package com.mkch.youshi.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,10 +16,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.mkch.youshi.R;
 import com.mkch.youshi.bean.User;
 import com.mkch.youshi.config.CommonConstants;
-import com.mkch.youshi.service.LocationService;
 import com.mkch.youshi.util.CommonUtil;
 
 import org.apache.http.conn.ConnectTimeoutException;
@@ -28,6 +35,7 @@ import org.xutils.x;
 import java.lang.ref.WeakReference;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 
 public class ReviseAddressActivity extends Activity {
 
@@ -35,10 +43,12 @@ public class ReviseAddressActivity extends Activity {
     private TextView mTvTitle;
     private TextView mTvPosition;
     private EditText mEtPosition;
-    private String position;
+    private String position1, position2;
+    private int clickID = 0;
     private Button mBtnConfirm;
     private User mUser;
-    private LocationService locationService;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
     private final int SDK_PERMISSION_REQUEST = 127;
     private String permissionInfo;
     private static ProgressDialog mProgressDialog = null;
@@ -47,7 +57,7 @@ public class ReviseAddressActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revise_address);
-//        getPersimmions();
+        getPersimmions();
         initView();
         initData();
         setListener();
@@ -64,172 +74,116 @@ public class ReviseAddressActivity extends Activity {
 
     private void initData() {
         mTvTitle.setText("修改地区");
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
     }
 
-//    @TargetApi(23)
-//    private void getPersimmions() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            ArrayList<String> permissions = new ArrayList<String>();
-//            /***
-//             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
-//             */
-//            // 定位精确位置
-//            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
-//            }
-//            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
-//            }
-//            /*
-//             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
-//			 */
-//            // 读写权限
-//            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-//                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
-//            }
-//            // 读取电话状态权限
-//            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
-//                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
-//            }
-//
-//            if (permissions.size() > 0) {
-//                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
-//            }
-//        }
-//    }
-//
-//    @TargetApi(23)
-//    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
-//        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
-//            if (shouldShowRequestPermissionRationale(permission)) {
-//                return true;
-//            } else {
-//                permissionsList.add(permission);
-//                return false;
-//            }
-//
-//        } else {
-//            return true;
-//        }
-//    }
-//
-//    @TargetApi(23)
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        // TODO Auto-generated method stub
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//    }
-//
-//    @Override
-//    protected void onStart() {
-//        // TODO Auto-generated method stub
-//        super.onStart();
-//        // -----------location config ------------
-//        locationService = ((MyApplication) getApplication()).locationService;
-//        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
-//        locationService.registerListener(mListener);
-//        //注册监听
-//        int type = getIntent().getIntExtra("from", 0);
-//        if (type == 0) {
-//            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
-//        } else if (type == 1) {
-//            locationService.setLocationOption(locationService.getOption());
-//        }
-//        locationService.start();// 定位SDK
-//    }
-//
-//    /***
-//     * Stop location service
-//     */
-//    @Override
-//    protected void onStop() {
-//        // TODO Auto-generated method stub
-//        locationService.unregisterListener(mListener); //注销掉监听
-//        locationService.stop(); //停止定位服务
-//        super.onStop();
-//    }
-//
-//    /**
-//     * 显示请求字符串
-//     *
-//     * @param str
-//     */
-//    public void logMsg(String str) {
-//        try {
-//            if (mTvPosition != null)
-//                mTvPosition.setText(str);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    /*****
-//     * @see copy funtion to you project
-//     * 定位结果回调，重写onReceiveLocation方法
-//     */
-//    private BDLocationListener mListener = new BDLocationListener() {
-//
-//        @Override
-//        public void onReceiveLocation(BDLocation location) {
-//            // TODO Auto-generated method stub
-//            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
-//                StringBuffer sb = new StringBuffer(256);
-//                sb.append("time : ");
-//                /**
-//                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
-//                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
-//                 */
-//                sb.append(location.getTime());
-//                sb.append("\ncity : ");// 城市
-//                sb.append(location.getCity());
-//                sb.append("\nDistrict : ");// 区
-//                sb.append(location.getDistrict());
-//                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
-//                    for (int i = 0; i < location.getPoiList().size(); i++) {
-//                        Poi poi = (Poi) location.getPoiList().get(i);
-//                        sb.append(poi.getName() + ";");
-//                    }
-//                }
-//                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-//                    sb.append("\nspeed : ");
-//                    sb.append(location.getSpeed());// 速度 单位：km/h
-//                    sb.append("\nsatellite : ");
-//                    sb.append(location.getSatelliteNumber());// 卫星数目
-//                    sb.append("\nheight : ");
-//                    sb.append(location.getAltitude());// 海拔高度 单位：米
-//                    sb.append("\ngps status : ");
-//                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
-//                    sb.append("\ndescribe : ");
-//                    sb.append("gps定位成功");
-//                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-//                    // 运营商信息
-//                    if (location.hasAltitude()) {// *****如果有海拔高度*****
-//                        sb.append("\nheight : ");
-//                        sb.append(location.getAltitude());// 单位：米
-//                    }
-//                    sb.append("\noperationers : ");// 运营商信息
-//                    sb.append(location.getOperators());
-//                    sb.append("\ndescribe : ");
-//                    sb.append("网络定位成功");
-//                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-//                    sb.append("\ndescribe : ");
-//                    sb.append("离线定位成功，离线定位结果也是有效的");
-//                } else if (location.getLocType() == BDLocation.TypeServerError) {
-//                    sb.append("\ndescribe : ");
-//                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-//                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-//                    sb.append("\ndescribe : ");
-//                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
-//                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-//                    sb.append("\ndescribe : ");
-//                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-//                }
-//                logMsg(sb.toString());
-//            }
-//        }
-//
-//    };
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * 显示请求字符串
+     *
+     * @param str
+     */
+    public void logMsg(String str) {
+        mTvPosition.setText(str);
+        mLocationClient.stop();
+    }
+
+    /*****
+     * 定位结果回调，重写onReceiveLocation方法
+     */
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                StringBuffer sb = new StringBuffer(256);
+                if (location.getProvince() == null || location.getProvince().equals("") || location.getProvince().equals("null")) {
+                    sb.append("定位失败，请点击重新获取");
+                } else {
+                    sb.append(location.getProvince().substring(0, location.getProvince().length() - 1));
+                    sb.append(location.getCity().substring(0, location.getCity().length() - 1));
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("服务端网络定位失败");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("无法获取有效定位依据，请重启手机");
+                }
+                logMsg(sb.toString());
+            }
+        }
+
+    }
 
     private void setListener() {
         mIvBack.setOnClickListener(new View.OnClickListener() {
@@ -238,15 +192,28 @@ public class ReviseAddressActivity extends Activity {
                 ReviseAddressActivity.this.finish();
             }
         });
+        mTvPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position1 = mTvPosition.getText().toString();
+                if (position1.equals("定位失败，请点击重新获取")) {
+                    mLocationClient.start();
+                } else {
+                    clickID = 0;
+                    ChangeAddressFromNet(position1);
+                }
+            }
+        });
         mBtnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                position = mEtPosition.getText().toString();
-                if (position == null || position.equals("")) {
+                position2 = mEtPosition.getText().toString();
+                if (position2 == null || position2.equals("")) {
                     Toast.makeText(ReviseAddressActivity.this, "您未填写地区", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                ChangeAddressFromNet();
+                clickID = 1;
+                ChangeAddressFromNet(position2);
             }
         });
     }
@@ -287,7 +254,11 @@ public class ReviseAddressActivity extends Activity {
     }
 
     public void updateUserInfo() {
-        mUser.setAddress(position);
+        if (clickID == 0) {
+            mUser.setAddress(position1);
+        } else {
+            mUser.setAddress(position2);
+        }
         CommonUtil.saveUserInfo(mUser, ReviseAddressActivity.this);
         Toast.makeText(this, "已修改", Toast.LENGTH_LONG).show();
         ReviseAddressActivity.this.finish();
@@ -296,15 +267,14 @@ public class ReviseAddressActivity extends Activity {
     /**
      * 修改用户地区
      */
-    private void ChangeAddressFromNet() {
+    private void ChangeAddressFromNet(final String place) {
         //弹出加载进度条
         mProgressDialog = ProgressDialog.show(ReviseAddressActivity.this, "请稍等", "正在修改中...", true, true);
         //使用xutils3访问网络并获取返回值
         RequestParams requestParams = new RequestParams(CommonConstants.ChangePlace);
         //包装请求参数
         String code = CommonUtil.getUserInfo(ReviseAddressActivity.this).getLoginCode();
-        position = mEtPosition.getText().toString();
-        String _req_json = "{\"Place\":\"" + position + "\"}";
+        String _req_json = "{\"Place\":\"" + place + "\"}";
         requestParams.addBodyParameter("", _req_json);//用户名
         requestParams.addHeader("sVerifyCode", code);//头信息
         x.http().post(requestParams, new Callback.CommonCallback<String>() {
