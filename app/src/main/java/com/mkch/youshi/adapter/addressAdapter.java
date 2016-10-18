3abb4a8930cfbd121d028870d77b3054ee2900d5
androidYZP <18626312413@163.com>
2016-10-18 13:07:59 +0800

    事件发起---选择地址完成

--- app/src/main/java/com/mkch/youshi/adapter/addressAdapter.java
+++ app/src/main/java/com/mkch/youshi/adapter/addressAdapter.java
@@ -6,6 +6,9 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.baidu.location.Poi;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.route.SuggestAddrInfo;
+import com.baidu.mapapi.search.sug.SuggestionResult;
 import com.mkch.youshi.R;
 import com.mkch.youshi.util.UIUtils;
 
@@ -17,19 +20,37 @@ import java.util.List;
 
 public class AddressAdapter extends BaseAdapter {
     List<Poi> list;
+    List<PoiInfo> InfoList;
+    List<SuggestionResult.SuggestionInfo> mSuggestList;
+    boolean isSuggest;
 
     public AddressAdapter(List<Poi> list) {
         this.list = list;
     }
 
+    public AddressAdapter(List<SuggestionResult.SuggestionInfo> SuggestList,boolean isSuggest) {
+        this.mSuggestList = SuggestList;
+        this.isSuggest = isSuggest;
+    }
+
+
     @Override
     public int getCount() {
-        return list.size();
+        if (isSuggest){
+            return mSuggestList.size();
+        }else {
+            return list.size();
+        }
     }
 
     @Override
     public Object getItem(int position) {
-        return list.get(position);
+        if (isSuggest){
+            return mSuggestList.get(position);
+        }else {
+
+            return list.get(position);
+        }
     }
 
     @Override
@@ -41,7 +62,13 @@ public class AddressAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View AddressView = UIUtils.inflate(R.layout.address_item);
         TextView tvAddress = (TextView) AddressView.findViewById(R.id.tv_address);
-        tvAddress.setText(list.get(position).getName());
+
+        if (isSuggest){
+            tvAddress.setText(mSuggestList.get(position).city
+                    +mSuggestList.get(position).district+mSuggestList.get(position).key);
+        }else {
+            tvAddress.setText(list.get(position).getName());
+        }
         return AddressView;
     }
 }
