b06d521c10d4df772041b067794ae2ead785ceba
androidYZP <18626312413@163.com>
2016-11-04 15:30:29 +0800

    个人习惯以及引导页面

--- app/src/main/java/com/mkch/youshi/fragment/TodayFragment.java
+++ app/src/main/java/com/mkch/youshi/fragment/TodayFragment.java
@@ -135,9 +135,6 @@ public class TodayFragment extends Fragment implements GestureDetector.OnGesture
     @Override
     public void onResume() {
         super.onResume();
-        mLapLine = 1;//初始化行数
-        initCircleAndLineTime(TimesUtils.getTime());//初始化圆盘和直线时间轴
-        mMonthChooseDate = ((MainActivity) getActivity()).getmMonthChooseDate();
         if (mMonthChooseDate != null) {//当有从日历月视图传过来数据时,重新初始化数据,并更新界面
             mLapLine = 1;//初始化行数
             initData();
@@ -145,6 +142,12 @@ public class TodayFragment extends Fragment implements GestureDetector.OnGesture
             //更新圆盘界面
             String chooseDate = TimesUtils.formatDate(currentYear, currentMonth, currentDay + "");
             initCircleAndLineTime(chooseDate);//初始化圆盘和直线时间轴
+        }else {
+            mLapLine = 1;//初始化行数
+            initCircleAndLineTime(TimesUtils.getTime());//初始化圆盘和直线时间轴
+            mMonthChooseDate = ((MainActivity) getActivity()).getmMonthChooseDate();
+            initData();
+            updateUI(); //刷新界面
         }
 
     }
